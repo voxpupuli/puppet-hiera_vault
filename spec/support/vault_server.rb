a56df15d81bdda901d09a9cdb72a4ae105e3067a
Tim Meusel <tim@bastelfreak.de>
2025-03-10 09:06:54 +0100

    rubocop: autofix

--- spec/support/vault_server.rb
+++ spec/support/vault_server.rb
@@ -15,15 +15,14 @@ module RSpec
   class VaultServer
     include Singleton
 
-    TOKEN_PATH = File.expand_path("~/.vault-token").freeze
+    TOKEN_PATH = File.expand_path('~/.vault-token').freeze
     TOKEN_PATH_BKUP = "#{TOKEN_PATH}.bak".freeze
 
     def self.method_missing(m, *args, &block)
-      self.instance.public_send(m, *args, &block)
+      instance.public_send(m, *args, &block)
     end
 
-    attr_reader :token
-    attr_reader :unseal_token
+    attr_reader :token, :unseal_token
 
     def initialize
       # If there is already a vault-token, we need to move it so we do not
@@ -35,11 +34,11 @@ module RSpec
         end
       end
 
-      io = Tempfile.new("vault-server")
-      pid = Process.spawn({}, "vault server -dev", out: io.to_i, err: io.to_i)
+      io = Tempfile.new('vault-server')
+      pid = Process.spawn({}, 'vault server -dev', out: io.to_i, err: io.to_i)
 
       at_exit do
-        Process.kill("INT", pid)
+        Process.kill('INT', pid)
         Process.waitpid2(pid)
 
         io.close
@@ -49,35 +48,30 @@ module RSpec
       wait_for_ready do
         @token = File.read(TOKEN_PATH)
 
-        output = ""
-        while
-          io.rewind
+        output = ''
+        while io.rewind
           output = io.read
-          break if !output.empty?
+          break unless output.empty?
         end
 
-        if output.match(/Unseal Key.*: (.+)/)
-          @unseal_token = $1.strip
-        else
-          raise "Vault did not return an unseal token!"
-        end
+        raise 'Vault did not return an unseal token!' unless output.match(%r{Unseal Key.*: (.+)})
+
+        @unseal_token = ::Regexp.last_match(1).strip
       end
     end
 
     def address
-      "http://127.0.0.1:8200"
+      'http://127.0.0.1:8200'
     end
 
-    def wait_for_ready(&block)
+    def wait_for_ready
       Timeout.timeout(10) do
-        while !File.exist?(TOKEN_PATH)
-          sleep(0.25)
-        end
+        sleep(0.25) until File.exist?(TOKEN_PATH)
       end
 
       yield
     rescue Timeout::Error
-      raise "Vault did not start in 10 seconds!"
+      raise 'Vault did not start in 10 seconds!'
     end
   end
 end
